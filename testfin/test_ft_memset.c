21a18ee74829e81702964629d15ba145b4d6f33a
Jeroen Jacobs <Jeroen@MacBook-Pro-van-Jeroen.local>
2020-11-01 23:03:09 +0100

    memset test met sizeof

--- testfin/test_ft_memset.c
+++ testfin/test_ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: jjacobs <marvin@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/26 18:40:05 by jjacobs       #+#    #+#                 */
-/*   Updated: 2020/10/28 22:41:34 by jjacobs          ###   ########.fr       */
+/*   Updated: 2020/10/29 19:23:22 by jjacobs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,51 @@
 
 int		main(void)
 {
-	char	str1[30] = "hallo";
-	char	str2[30] = "hallo";
-	char	c;
-	int		len;
+	char	str1[6] = "hallo";
+	char	str2[6] = "hallo";
+	int		c;
+	size_t	len;
+	
+	printf("What is sizeof(char)? It is %lu\n", sizeof(char));
+	printf("What is sizeof(int)? It is %lu\n", sizeof(int));
 
-	c = 'q';
-	len = 2;
+	c = 'k';
+	len = 1;
 
-	printf("Before memset, str: %s\n", str1);
-	memset(str1, c, len);
-	printf("After memset, str: %s\n", str1);
-	printf("Before memset, str: %s\n", str2);
-	memset(str2, c, len);
-	printf("After memset, str: %s\n", str2);
+	printf("Before memset(): %s\n", str1);
+	memset(str1 + 5, c, len);
+	printf("After memset(): %s\n", str1);
+	printf("Before ft_memset(): %s\n", str2);
+	ft_memset(str2 + 5, c, len);
+	printf("After ft_memset(): %s\n (shows more random stuff, since termining 0 is set to k by memset)\n", str2);
+	
+	char	str3[1000] = "haha";
+	char	str4[1000] = "haha";
+
+	len = 20;
+	c = ('j' - 257 - 1000) % 256;
+	
+	printf("   %3d - %c\n", 129, 129);
+	printf("Before memset(): %s\n", str3);
+	memset(str3, c, len);
+	printf("After memset(): %s\n", str3);
+	printf("Before ft_memset(): %s\n", str4);
+	ft_memset(str4, c, len);
+	printf("After ft_memset(): %s\n", str4);
+	
+	int		arr1[10];
+	int		arr2[10];
+
+	len = 2*sizeof(int);
+	c = -1;
+	
+	printf("Before memset(), arr[3]: %d\n", arr1[3]);
+	memset(arr1, c, len);
+	printf("After memset(), arr[3]: %d\n", arr1[3]);
+	printf("Before ft_memset(), arr[3]: %d\n", arr2[3]);
+	ft_memset(arr2, c, len);
+	printf("After ft_memset(), arr[3]: %d\n", arr2[3]);
+	
+	printf("%c%c%c\n", 226, 152, 131);
+    return 0;
 }
