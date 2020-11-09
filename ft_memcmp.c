051f9e0a72556938a0266ebdddcb63e739a4f067
Jeroen c Jacobs <jjacobs@f0r3s26.codam.nl>
2020-11-09 22:35:55 +0100

    draft memcmp, wel stuk nu

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,22 +6,20 @@
 /*   By: jjacobs <marvin@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/09 22:22:24 by jjacobs       #+#    #+#                 */
-/*   Updated: 2020/11/09 22:22:40 by jjacobs       ########   odam.nl         */
+/*   Updated: 2020/11/09 22:35:19 by jjacobs       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-int		ft_memcmp(const char *s1, const char *s2, size_t n)
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
+	//Rules voor result bedenken - byte wise comp?
+	
 	while ((*s1 != '\0' || *s2 != '\0') && n-- > 0)
 	{
 		if (*s1 != *s2)
 			return (*s1 - *s2);
-		if (*s1 != '\0')
-			s1++;
-		if (*s2 != '\0')
-			s2++;
 	}
 	return (0);
 }
